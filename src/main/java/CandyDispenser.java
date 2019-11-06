24dd03a601b0ad25d02a2694839adb44d9fc3cb5
SIlvioVasconcelos <silvio.vasconcelos@kaibee.fr>
2019-11-06 21:11:06 +0100

    second commit - end of kata

--- src/main/java/CandyDispenser.java
+++ src/main/java/CandyDispenser.java
@@ -1,30 +1,52 @@
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CandyDispenser {
 
-  public static final String TRICK_OR_TREAT = "Trick or treat!";
-  public static final String THANK_YOU_STRANGE_UNCLE = "Thank you, strange uncle!";
-  public static final String CANDY = "candy";
-  public static final String APPLE = "apple";
-  public static final String BOMB = "bomb";
-  public static final int minimumOfCandies = 2;
+    public static final String TRICK_OR_TREAT = "Trick or treat!";
+    public static final String THANK_YOU_STRANGE_UNCLE = "Thank you, strange uncle!";
+    public static final String CANDY = "candy";
+    public static final String APPLE = "apple";
+    public static final String BOMB = "bomb";
+    private static final int minimumOfCandies = 2;
 
-  public String trickOrTreat(int numberOfChild, List<List<String>> candies) {
-
-    if (containsBomb(candies)) {
-      return TRICK_OR_TREAT;
+    public String trickOrTreat(int numberOfChild, List<List<String>> candies) {
+        if (containsBomb(candies)
+                || rightNumberOfPackOfCandies(numberOfChild, candies)
+                || notSameNumberOfCandiesByPack(candies)
+                || notEnoughCandies(numberOfChild, candies)) {
+            return TRICK_OR_TREAT;
+        }
+        return THANK_YOU_STRANGE_UNCLE;
     }
 
-    final long numberOfHappyChild = candies.stream().filter(this::isHappy).count();
-    return numberOfHappyChild == numberOfChild ? THANK_YOU_STRANGE_UNCLE : TRICK_OR_TREAT;
-  }
+    private boolean notEnoughCandies(int numberOfChild, List<List<String>> candies) {
+        final long numberOfHappyChild = candies.stream().filter(this::isHappy).count();
+        return numberOfHappyChild != numberOfChild;
+    }
 
-  private boolean containsBomb(List<List<String>> candies) {
-    return candies.stream().flatMap(Collection::stream).anyMatch(s -> s.contains(BOMB));
-  }
+    private boolean notSameNumberOfCandiesByPack(List<List<String>> packOfCandies) {
+        Set<Long> numberOfCandies = new HashSet<>();
+        packOfCandies.forEach(candies ->
+                numberOfCandies.add(getNumberOFCandies(candies)));
+        return numberOfCandies.size() != 1;
+    }
 
-  private boolean isHappy(List<String> candies) {
-    return (minimumOfCandies <= candies.stream().filter(s -> s.contains(CANDY)).count());
-  }
+    private boolean rightNumberOfPackOfCandies(int numberOfChild, List<List<String>> candies) {
+        return numberOfChild != candies.size();
+    }
+
+    private boolean containsBomb(List<List<String>> candies) {
+        return candies.stream().flatMap(Collection::stream).anyMatch(s -> s.contains(BOMB));
+    }
+
+    private boolean isHappy(List<String> candies) {
+        return (minimumOfCandies <= getNumberOFCandies(candies));
+    }
+
+    private long getNumberOFCandies(List<String> candies) {
+        return candies.stream().filter(s -> s.contains(CANDY)).count();
+    }
 }
--- src/test/java/CandyDispenserTest.java
+++ src/test/java/CandyDispenserTest.java
@@ -5,54 +5,71 @@ import java.util.Arrays;
 
 public class CandyDispenserTest {
 
-  private CandyDispenser candyDispenser = new CandyDispenser();
+    private CandyDispenser candyDispenser = new CandyDispenser();
 
-  @Test
-  public void child_accept_minimum_2_candies() {
-    String response = candyDispenser.trickOrTreat(1, Arrays.asList(Arrays.asList(CandyDispenser.CANDY)));
-    assertTrickOrTreat(response);
-  }
+    @Test
+    public void child_accept_minimum_2_candies() {
+        String response = candyDispenser.trickOrTreat(1, Arrays.asList(Arrays.asList(CandyDispenser.CANDY)));
+        assertTrickOrTreat(response);
+    }
 
-  @Test
-  public void child_accept_only_candies() {
-    String response = candyDispenser.trickOrTreat(1,
-        Arrays.asList(Arrays.asList(CandyDispenser.CANDY, CandyDispenser.APPLE, CandyDispenser.APPLE)));
-    assertTrickOrTreat(response);
-  }
+    @Test
+    public void child_accept_only_candies() {
+        String response = candyDispenser.trickOrTreat(1,
+                Arrays.asList(Arrays.asList(CandyDispenser.CANDY, CandyDispenser.APPLE, CandyDispenser.APPLE)));
+        assertTrickOrTreat(response);
+    }
 
-  @Test
-  public void child_is_satisfied_with_2_or_more_candies() {
-    String response = candyDispenser.trickOrTreat(1,
-        Arrays.asList(Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY, CandyDispenser.APPLE)));
-    assertThankYou(response);
-  }
+    @Test
+    public void child_is_satisfied_with_2_or_more_candies() {
+        String response = candyDispenser.trickOrTreat(1,
+                Arrays.asList(Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY, CandyDispenser.APPLE)));
+        assertThankYou(response);
+    }
 
-  @Test
-  public void child_is_not_satisfied_with_bomb() {
-    String response = candyDispenser.trickOrTreat(1,
-        Arrays.asList(Arrays.asList(CandyDispenser.BOMB, CandyDispenser.CANDY, CandyDispenser.CANDY)));
-    assertTrickOrTreat(response);
-  }
+    @Test
+    public void child_is_not_satisfied_with_bomb() {
+        String response = candyDispenser.trickOrTreat(1,
+                Arrays.asList(Arrays.asList(CandyDispenser.BOMB, CandyDispenser.CANDY, CandyDispenser.CANDY)));
+        assertTrickOrTreat(response);
+    }
 
-  @Test
-  public void children_should_get_candies() {
-    String response = candyDispenser.trickOrTreat(2,
-        Arrays.asList(Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY, CandyDispenser.CANDY), Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY)));
-    assertThankYou(response);
-  }
+    @Test
+    public void children_should_get_candies() {
+        String response = candyDispenser.trickOrTreat(2,
+                Arrays.asList(Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY), Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY)));
+        assertThankYou(response);
+    }
 
-  @Test
-  public void children_should_be_happy() {
-    String response = candyDispenser.trickOrTreat(2, Arrays.asList(Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY, CandyDispenser.CANDY),
-            Arrays.asList(CandyDispenser.APPLE, CandyDispenser.CANDY)));
-    assertTrickOrTreat(response);
-  }
+    @Test
+    public void children_should_be_happy() {
+        String response = candyDispenser.trickOrTreat(2, Arrays.asList(Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY, CandyDispenser.CANDY),
+                Arrays.asList(CandyDispenser.APPLE, CandyDispenser.CANDY)));
+        assertTrickOrTreat(response);
+    }
 
-  private void assertThankYou(String response) {
-    Assert.assertEquals(CandyDispenser.THANK_YOU_STRANGE_UNCLE, response);
-  }
+    @Test
+    public void candies_should_be_equals_to_children() {
+        assertTrickOrTreat(candyDispenser.trickOrTreat(2, Arrays.asList(Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY, CandyDispenser.CANDY),
+                Arrays.asList(CandyDispenser.CANDY, CandyDispenser.APPLE), Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY))));
+    }
 
-  private void assertTrickOrTreat(String response) {
-    Assert.assertEquals(CandyDispenser.TRICK_OR_TREAT, response);
-  }
+    @Test
+    public void all_child_should_get_same_number_of_candies() {
+        assertTrickOrTreat(candyDispenser.trickOrTreat(3, Arrays.asList(Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY, CandyDispenser.CANDY),
+                Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY), Arrays.asList(CandyDispenser.CANDY, CandyDispenser.CANDY))));
+    }
+
+    @Test
+    public void integration_test() {
+        assertTrickOrTreat(candyDispenser.trickOrTreat(3, Arrays.asList(Arrays.asList("candy", "apple", "candy"), Arrays.asList("candy", "candy"), Arrays.asList("candy", "bomb", "candy"))));
+    }
+
+    private void assertThankYou(String response) {
+        Assert.assertEquals(CandyDispenser.THANK_YOU_STRANGE_UNCLE, response);
+    }
+
+    private void assertTrickOrTreat(String response) {
+        Assert.assertEquals(CandyDispenser.TRICK_OR_TREAT, response);
+    }
 }
